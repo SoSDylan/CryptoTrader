66fecc1722a019c32c3506db941531cc57515e92
Dylan Larsen <dylanlarsen80@gmail.com>
2021-11-12 17:48:53 +1100

    Fixed some code formatting

--- CryptoTrader/Core/DownloadManager.cs
+++ CryptoTrader/Core/DownloadManager.cs
@@ -37,19 +37,17 @@ namespace CryptoTrader.Core
                 
                 foreach (var jsonCandle in json)
                 {
-                    long openTime = long.Parse(jsonCandle[0].ToString()); // Open time
-                    double open = double.Parse(jsonCandle[1].ToString()); // Open
-                    double high = double.Parse(jsonCandle[2].ToString()); // High
-                    double low = double.Parse(jsonCandle[3].ToString()); // Low
-                    double close = double.Parse(jsonCandle[4].ToString()); // Close
-                    double volume = double.Parse(jsonCandle[5].ToString()); // Volume
-                    long closeTime = long.Parse(jsonCandle[6].ToString()); // Close time
-                    double quoteAssetVolume = double.Parse(jsonCandle[7].ToString()); // Quote asset volume
-                    int numberOfTrades = int.Parse(jsonCandle[8].ToString()); // Number of trades
-                    double takerBuyBaseAssetVolume =
-                        double.Parse(jsonCandle[9].ToString()); // Taker buy base asset volume
-                    double takerBuyQuoteAssetVolume =
-                        double.Parse(jsonCandle[10].ToString()); // Taker buy quote asset volume
+                    long   openTime                 = long.Parse(jsonCandle[0].ToString()); // Open time
+                    double open                     = double.Parse(jsonCandle[1].ToString()); // Open
+                    double high                     = double.Parse(jsonCandle[2].ToString()); // High
+                    double low                      = double.Parse(jsonCandle[3].ToString()); // Low
+                    double close                    = double.Parse(jsonCandle[4].ToString()); // Close
+                    double volume                   = double.Parse(jsonCandle[5].ToString()); // Volume
+                    long   closeTime                = long.Parse(jsonCandle[6].ToString()); // Close time
+                    double quoteAssetVolume         = double.Parse(jsonCandle[7].ToString()); // Quote asset volume
+                    int    numberOfTrades           = int.Parse(jsonCandle[8].ToString()); // Number of trades
+                    double takerBuyBaseAssetVolume  =  double.Parse(jsonCandle[9].ToString()); // Taker buy base asset volume
+                    double takerBuyQuoteAssetVolume = double.Parse(jsonCandle[10].ToString()); // Taker buy quote asset volume
 
                     var candle = new Candle(openTime,
                                             open,
